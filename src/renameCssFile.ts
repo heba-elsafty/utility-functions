b8b73d2b7c8fb0ce09a76291b9023b347acd85a2
heba-elsafty <heba.m.elsaft@gmail.com>
2024-08-12 16:21:58 +0300

    ✍️ Document the renameCssFile function.

--- src/renameCssFile.ts
+++ src/renameCssFile.ts
@@ -1,6 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
+
+/**
+ * Renames a CSS file from its original name to a new name within the specified directory.
+ * This function is particularly useful in build processes where you need to rename output files dynamically.
+ * 
+ * @param {string} oldFilename - The current name of the file you want to rename.
+ * @param {string} newFilename - The new name you want to give to the file ex: filename.module.min.css.
+ * 
+ * @example
+ * import { renameCssFile } from './utils/renameCssFile';
+ * 
+ * renameCssFile('filename.module.css', 'filename.module.min.css');
+ * 
+ * @function
+ * @name renameCssFile
+ */
+
 export function renameCssFile(oldFilename: string, newFilename: string): void {
     const oldPath = path.join(process.cwd(), 'dist', 'styles', oldFilename);
     const newPath = path.join(process.cwd(), 'dist', 'styles', newFilename);
